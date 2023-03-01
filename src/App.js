ae903a4d5c5af07c10525b1ac9391d5418f2d1f5
laraib shuja <105169013+l4r4ib@users.noreply.github.com>
2023-03-01 23:29:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state= {
+        monsters: [
+          {
+            name:'aditya'
+          },
+          {
+            name:'aditya'
+          },
+          {
+            name:'aditya'
+          },
+          {
+            name:'aditya'
+          },
+        ]
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+      {this.state.monsters.map((monster)=> {
+          return <h1> {monster.name} </h1> 
+      })}
+      </div>
+    );
+  }
+ 
+}
+
+export default App;
