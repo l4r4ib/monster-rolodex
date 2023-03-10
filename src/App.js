5534ee03732c51eb936db190222ee36c637cd7c7
laraib shuja <105169013+l4r4ib@users.noreply.github.com>
2023-03-11 02:23:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ class App extends Component {
     this.state= {
         monsters: [ ]
     }
-    console.log('constructor');
+    //console.log('state');
   }
    componentDidMount(){
-     console.log('componentDidMount');
+     //console.log('componentdidmount');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response)=> response.json())
     .then((users)=>
@@ -27,9 +27,22 @@ class App extends Component {
    }
 
   render(){
-    console.log(render);
+    //console.log('render');
     return (
       <div className="App">
+        <input className='search-box' type='search' placeholder='search here'
+        onChange={ (event)=> {
+          console.log(event.target.value)
+          const searchString = event.target.value.toLocaleLowerCase();
+          const filteredMonsters = this.state.monsters.filter((monster)=> {
+            return monster.name.toLocaleLowerCase().includes(searchString);
+          });
+
+        this.setState(()=> {
+          return { monsters: filteredMonsters }
+        })
+        }}
+         />
       {this.state.monsters.map((monster)=> {
           return <div key= {monster.id}> <h1> {monster.name} </h1>  </div>
       })}
