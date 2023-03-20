7e3b08a85c401fc4626462f87185e2951b4c3848
laraib shuja <105169013+l4r4ib@users.noreply.github.com>
2023-03-21 00:37:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,34 +21,34 @@ class App extends Component {
       ()=> {
       return { monsters: users};
     },
-    ()=> {
-      console.log(this.state)
-    })
+    )
     )
    }
+   onSearchChange = (event)=> {
+    console.log(event.target.value)
+    const searchField = event.target.value.toLocaleLowerCase();
+    
+  this.setState(()=> {
+    return { searchField }
+  })
+  }
 
   render(){
-   
-    const filteredMonsters = this.state.monsters.filter((monster)=> {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const {monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster)=> {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
     return (
       <div className="App">
         <input className='search-box' type='search' placeholder='search here'
-        onChange={ (event)=> {
-          console.log(event.target.value)
-          const searchField = event.target.value.toLocaleLowerCase();
-          
-        this.setState(()=> {
-          return { searchField }
-        })
-        }}
+        onChange={onSearchChange }
          />
       {filteredMonsters.map((monster)=> {
           return <div key= {monster.id}> <h1> {monster.name} </h1>  </div>
       })}
       </div>
-     
     );
   }
  
