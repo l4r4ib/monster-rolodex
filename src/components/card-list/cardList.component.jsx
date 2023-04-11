a2479c5e071876613cd2fba9057b77e4ee9e5420
laraib shuja <105169013+l4r4ib@users.noreply.github.com>
2023-04-11 08:47:19 +0530

    Update cardList.component.jsx

--- src/components/card-list/cardList.component.jsx
+++ src/components/card-list/cardList.component.jsx
@@ -2,8 +2,18 @@ import { Component } from "react";
 
 class CardList extends Component {
     render () {
-        console.log('yeah this shit works')
+
+        const {monsters} = this.props;
+
+       return ( 
+
+       <div> {monsters.map((monster) => (
+        <h1 key ={monster.id}>  {monster.name} </h1>
+       ))} </div> 
+
+       )
+       
     }
 }
 
-export default CardList
+export default CardList;
